5150cb1c9cb08ac566651ee5769f8486ae89314f
Taksuma <drexercise46@gmail.com>
2025-07-12 22:10:07 +0700

    Updated

--- koperasi_api/settings.py
+++ koperasi_api/settings.py
@@ -173,7 +173,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-CSRF_TRUSTED_ORIGINS= ["http://127.0.0.1:8000"]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
